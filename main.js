c3c315a784542ce32f973e2a13cdd70ccd4f7ba3
George1712 <84546695+George1712@users.noreply.github.com>
2021-10-09 20:32:20 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,124 @@
+// Landing ----------------------------------------------
+// Fixed NavBar After Scroll
+let nav = document.getElementById("nav");
+//
+window.addEventListener("scroll", function () {
+  if (scrollY >= 140) {
+    nav.style.cssText = "position: fixed; top: 0; background-color: #202020";
+  } else {
+    nav.style.cssText = "position: relative; background-color: none";
+  }
+});
+// Make Toggle Menu Show Links Onclick
+let toggle = document.getElementById("toggle");
+let links = document.getElementById("links");
+//
+toggle.addEventListener("click", function () {
+  toggle.classList.toggle("active-toggle");
+  if (toggle.classList.contains("active-toggle")) {
+    links.style.display = "flex";
+  } else {
+    links.style.display = "none";
+  }
+});
+// Give Nav-Links Active Class Onclick
+let navLinks = links.querySelectorAll("li a");
+//
+for (let i = 0; i < navLinks.length; i++) {
+  navLinks[i].addEventListener("click", function () {
+    let activeLink = document.getElementsByClassName("active-link");
+    activeLink[0].classList.remove("active-link");
+    if (activeLink.length < 1) {
+      navLinks[i].classList.add("active-link");
+    }
+  });
+}
+
+// About ----------------------------------------------
+// Control Shown Advantages By Adv-Boxes
+let advBox = document.querySelectorAll(".adv-box");
+let advContent = document.querySelectorAll(".adv-content");
+//
+for (let i = 0; i < advBox.length; i++) {
+  advBox[i].addEventListener("click", function () {
+    let active = document.getElementsByClassName("active-adv");
+    active[0].classList.remove("active-adv");
+    if (active.length < 1) {
+      advBox[i].classList.add("active-adv");
+    }
+    //
+    let showen = document.getElementsByClassName("showen-adv");
+    showen[0].classList.remove("showen-adv");
+    if (showen.length < 1) {
+      advContent[i].classList.add("showen-adv");
+    }
+  });
+}
+
+// Services ----------------------------------------------
+// Control Showen Paragraphs By Boxes
+let chooseBox = document.querySelectorAll(".choose-boxes div");
+let chooseContent = document.querySelectorAll(".choose-content p");
+//
+for (let i = 0; i < chooseBox.length; i++) {
+  chooseBox[i].addEventListener("click", function () {
+    let activeBox = document.getElementsByClassName("active-box");
+    activeBox[0].classList.remove("active-box");
+    if (activeBox.length < 1) {
+      chooseBox[i].classList.add("active-box");
+    }
+    //
+    let activeP = document.getElementsByClassName("active-p");
+    activeP[0].classList.remove("active-p");
+    if (activeP.length < 1) {
+      chooseContent[i].classList.add("active-p");
+    }
+  });
+}
+
+// Works ----------------------------------------------
+// Control Showen Images By Boxes
+let workBox = document.querySelectorAll(".work-boxes .work-box");
+let picsBox = document.querySelectorAll(".work-pics .pics");
+//
+for (let i = 0; i < workBox.length; i++) {
+  workBox[i].addEventListener("click", function () {
+    let activeWork = document.getElementsByClassName("active-work");
+    activeWork[0].classList.remove("active-work");
+    if (activeWork.length < 1) {
+      workBox[i].classList.add("active-work");
+    }
+    //
+    let activePics = document.getElementsByClassName("active-pics");
+    activePics[0].classList.remove("active-pics");
+    if (activePics.length < 1) {
+      picsBox[i].classList.add("active-pics");
+    }
+  });
+}
+// Make Love React on Pics
+let capIcons = document.querySelector(".work-pics");
+let love = capIcons.getElementsByClassName("fa-heart");
+//
+for (let i = 0; i < love.length; i++) {
+  love[i].addEventListener("click", function () {
+    love[i].style.cssText = "color: red;";
+  });
+}
+
+// Scroll To Top Button ----------------------------------------------
+let scrollUp = document.querySelector(".scroll-up");
+//
+window.addEventListener("scroll", function () {
+  if (scrollY >= 500) {
+    scrollUp.style.display = "block";
+  } else {
+    scrollUp.style.display = "none";
+  }
+});
+//
+scrollUp.addEventListener("click", function () {
+  window.scrollTo(0, 0);
+});
+// 
+// I Felt In Love With This Programming Lang
